eddc883f5342c3ee4063332d587e57f0754babd1
IlanTauscher <48240500@est.ort.edu.ar>
2025-04-04 20:36:20 -0300

    terminado

--- index.js
+++ index.js
@@ -35,8 +35,55 @@
 
 // Ejercicio 5:
 
-let array = [4, 5, 1, 3, 10]
+// let array = [4, 5, 1, 3, 10]
 
-let funcionFlecha = (array) => {
-    
-};
+// let funcionFlecha = (array) => {
+//     let sumar = 0
+//     for(i = 0;i<array.length; i++){
+//         sumar += array[i]
+//     }
+//     console.log(sumar)
+// };
+
+// let mostrar = funcionFlecha(array)
+
+// Ejericio 6:
+
+// let array = [4, 5, 1, 3, 10]
+
+// let funcionFlecha = (array) => {
+//     array.reverse()
+//     console.log(array)
+// };
+
+// let mostrar = funcionFlecha(array)
+
+// Ejercicio 7:
+
+// let array = [4, 5, 1, 3, 7]
+
+// let funcionFlecha = (array) => {
+//     array.sort()
+//     console.log(array)
+// };
+
+// let mostrar = funcionFlecha(array)
+
+// Ejercicio 8:
+
+// let array = [4, 5, 1, 3, 7];
+
+// let funcionFlecha = (array) => {
+//     let filtrado = array.filter(x => x < 5);
+//     console.log(filtrado);
+// };
+
+// let mostrar = funcionFlecha(array);
+
+// Ejercicio 9:
+
+// let array = [4, 5, 7, 1, 7, 3, 7];
+
+// let veces = array.filter(f => f === 7).length;
+
+// console.log(veces);
