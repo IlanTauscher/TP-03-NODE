f6d40a8d32eeb676e46b917b5f0f615a51bf8252
ILAN TAUSCHER <48240500@ort>
2025-04-04 12:03:29 -0300

    Avances hasta el ejericio 5

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("hola mundo");
+// Ejericio 1:
+// console.log("hola mundo!");
+
+//Ejercicio 2:
+// let array = ["ilan", "matias", "jose"];
+
+// let tradicional = funcion(array);
+
+// let funcionFlecha = (array) => console.log(`Flecha: ${array}`);
+
+// let flecha = funcionFlecha(array);
+
+// function funcion(array){
+//     console.log(`Normal: ${array}`)
+// }
+
+// Ejericio 3:
+// let array = ["ilan", "matias", "jose"];
+
+// let funcionFlechaArgergar = (array) => array.push('federico');
+
+// let funcionFlecha = (array) => console.log(`Flecha: ${array}`);
+
+// let sumar = funcionFlechaArgergar(array);
+
+// let flecha = funcionFlecha(array);
+
+// Ejericio 4:
+
+// let array = ["ilan", "matias", "jose"];
+
+// let mostrar = array.lastIndexOf("jose");
+
+// console.log(mostrar);
+
+// Ejercicio 5:
+
+let array = [4, 5, 1, 3, 10]
+
+let funcionFlecha = (array) => {
+    
+};
